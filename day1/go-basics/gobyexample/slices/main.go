acd5cb162c9bda499fdd6cf3c059f4a8c1ddc46e
maxchichar <maxwellchibuezecharles@gmail.com>
2026-04-08 10:49:11 +0100

    gobyexample: slice done

--- day1/go-basics/gobyexample/slices/main.go
+++ day1/go-basics/gobyexample/slices/main.go
@@ -1,7 +1,8 @@
 package main
 
-import(
+import (
 	"fmt"
+	"slices"
 )
 
 func main()  {
@@ -21,4 +22,32 @@ func main()  {
 	fmt.Println("get:", a[1])
 
 	fmt.Println(len(a))
+
+	a = append(a, "Lucky")
+	a = append(a, "Greatman", "Ola")
+	fmt.Println("apd:", a)
+
+	c := make([]string, len(a))
+	copy(c, a)
+	fmt.Println("cpy:", c)
+
+	l := a[2:5]
+	fmt.Println("sl1:",l)
+
+	l = a[:5]
+	fmt.Println("sl2:",l)
+
+	l = a[2:]
+	fmt.Println("sl3:",l)
+	
+	t := []string{"a", "b", "c", "d"}
+	fmt.Println("dlc:", t)
+
+	t2 := []string{"a", "b", "c", "e"}
+	if slices.Equal(t, t2) {
+		fmt.Println("t == t2")
+	}else{
+		fmt.Println("t != t2")
+	}
+
 }
