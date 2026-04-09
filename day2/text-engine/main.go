4ff2d7a4a706dad0bdb98a529f9865eb1123fd40
maxchichar <maxwellchibuezecharles@gmail.com>
2026-04-09 04:19:16 +0100

    using os to read and write os.O_RDWR| create files os.O_CREATE and perm for permission i used 0664

--- day2/text-engine/main.go
+++ day2/text-engine/main.go
@@ -12,14 +12,29 @@ func main()  {
 		os.Exit(1)
 	}
 
-	inputFile := os.Args[2]
-	outputFile := os.Args[3]
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
 
 	if inputFile == outputFile {
 		log.Fatal("✗ Both file name cannot be the same")
 	}
 	
+	file, err := os.OpenFile("sample.txt", os.O_RDWR|os.O_CREATE, 0664)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := file.Close(); err != nil{ // we automatically close at the end of our program
+		log.Fatal(err)
+	}
 	
+	
+	file2, err := os.OpenFile("result.txt", os.O_RDWR|os.O_CREATE, 0664)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := file2.Close(); err != nil{ // we automatically close at the end of our program
+		log.Fatal(err)
+	}
 
 }
 
